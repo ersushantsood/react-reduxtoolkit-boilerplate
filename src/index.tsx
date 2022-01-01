4ebd95cb21c8ae3ee90d4a41af8264233f353ee1
ersushantsood@gmail.com <ersushantsood@gmail.com>
2022-01-01 11:40:30 -0800

    added counterSlice sample for Redux-toolkit

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
+// Adding store to the app
+import { store } from './redux/store'
+
+const rootElement = document.getElementById("root");
 ReactDOM.render(
-  
-    <App name="root"/>,
-  document.getElementById('root')
+  <StrictMode>
+    <Provider store={store}>    
+      <App name="root"/>,
+      document.getElementById('root')
+    </Provider>
+  </StrictMode>,
+  rootElement
 );
 
 // If you want your app to work offline and load faster, you can change
